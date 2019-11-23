f715c18df8b6b6e3306488d6fa5ac641cea54a29
linkerbs <00002918@uca.edu.sv>
2019-11-23 08:48:22 -0600

    Finalizacion del modelo del cliente

---
+++ application/models/Plan_model.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+class Plan_model extends CI_Model {
+ 
+ var $API ="";
+ function __construct() {
+ parent::__construct();
+ $this->API="http://localhost/ApiInternet/index.php/";
+ }
+ 
+ function list_plan(){
+ return json_decode($this->restclient->get());
+ }
+ 
+ function plan($id){
+ $params = array('Id'=>  $id);
+ return json_decode($this->restclient->get($params),true);
+ }
+ 
+ function save($array)
+ {
+ $this->restclient->post($array);
+ }
+ 
+ function update($array)
+ {
+ $this->restclient->put($array);
+ }
+ 
+ function delete($id)
+ {
+ $this->restclient->delete($id);
+ }
+}
+?>
